0df857b85f854d512fffffb406b7afcd60126296
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2022-05-30 06:15:07 +0700

    feat: add Chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { UserContext } from "../lib/context";
+import { useUserData } from "../lib/hooks";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const userData = useUserData();
+  return (
+    <UserContext.Provider value={userData}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </UserContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
