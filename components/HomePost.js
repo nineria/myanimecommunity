a49be40d74f97fba1a11003cca59f495e0ba40f5
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2022-05-30 17:36:21 +0700

    feat: Posts in Home page
    Post can now be edited

--- components/HomePost.js
+++ components/HomePost.js
@@ -1,101 +1,123 @@
-import React, { useState } from "react";
-import { ChevronUp, Messages } from "tabler-icons-react";
+import React, { useState, useEffect } from "react";
+import { ChevronUp, Messages, Edit, SquareX } from "tabler-icons-react";
 import Link from "next/link";
+import ManageHomePost from "./ManageHomePost";
+import { Container } from "@chakra-ui/react";
+import { Animate } from "react-simple-animate";
 
-export default function HomePost(props) {
+export default function HomePost({ dummyData }) {
   const [toggle, setTogle] = useState(true);
 
-  return (
-    <div className="rounded-sm bg-[#242629] w-full ">
-      <h2
-        className={`flex flex-row justify-between items-center bg-[#ec5555] py-1 px-3 font-bold text-md text-white ${
-          toggle === true ? "rounded-t-sm" : "rounded-sm"
-        }`}
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const postData = dummyData;
+
+  const HandleOpenMenu = () => {
+    return setOpenMenu(!openMenu);
+  };
+
+  const HandlePostDataChange = (data) => {
+    console.log(data);
+  };
+
+  const Content = ({ header, headerLink, body }) => {
+    return (
+      <div
+        className={`flex lg:flex-row flex-col lg:justify-between lg:items-center px-3 py-2 text-[#ec5555] bg-[#242629]`}
       >
-        {props.children[0]}
-        <div onClick={() => setTogle(!toggle)}>
-          <ChevronUp
-            size={22}
-            className={`${
-              toggle === false ? "rotate-180" : ""
-            } cursor-pointer transition-all`}
-          />
+        <div className="flex gap-2">
+          <Messages size={45} />
+          <div className={`${body ? "flex  flex-col" : "flex items-center"}`}>
+            <div className="flex flex-row justify-start gap-2 items-center ">
+              <Link href={headerLink}>
+                <a className="truncate text-white cursor-pointer hover:underline w-fit">
+                  {header}
+                </a>
+              </Link>
+              <div className="px-1 bg-[#ec5555] w-fit h-fit rounded-sm text-white text-xs">
+                ใหม่
+              </div>
+            </div>
+            <p className="truncate w-full text-[#aaa] text-xs">{body}</p>
+          </div>
         </div>
-      </h2>
-      <div className="flex flex-col bg-[#aaa] gap-[1px]">
-        {toggle === true ? props.children.slice(1) : null}
+        <div className="flex flex-row gap-4 text-white">
+          <div className="flex lg:flex-col lg:items-center items-end gap-1">
+            <div>25</div>
+            <div className="text-sm text-[#aaa]">โพสต์</div>
+          </div>
+          <div className="lg:border-r-[1px] lg:border-[#aaa]" />
+          <div className="flex lg:flex-col lg:items-center items-end gap-1">
+            <div>3.6K</div>
+            <div className="text-sm text-[#aaa]">ข้อความ</div>
+          </div>
+          <div className="flex lg:flex-col lg:items-center items-end gap-1">
+            <div>แก้ไขล่าสุด</div>
+            <div className="text-sm text-[#aaa]">28/5/2565 - Admin</div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  };
 
-export function Title(props) {
   return (
-    <Link href={props.link}>
-      <a>
-        <span className="cursor-pointer hover:underline">{props.children}</span>
-      </a>
-    </Link>
-  );
-}
-
-export function Body(props) {
-  return (
-    <div
-      className={`flex lg:flex-row flex-col lg:justify-between lg:items-center px-3 py-2 text-[#ec5555] bg-[#242629]`}
-    >
-      <div className="flex gap-2">
-        <Messages size={45} />
-        <div
-          className={`${
-            props.children[1] ? "flex  flex-col" : "flex items-center"
-          }`}
+    <div>
+      {openMenu === true ? (
+        <ManageHomePost
+          handleOpenMenu={HandleOpenMenu}
+          open={openMenu}
+          postData={postData}
+          handlePostDataChange={HandlePostDataChange}
+        />
+      ) : null}
+      <Container maxW="container.xl">
+        <Animate
+          play
+          start={{
+            transform: "translateY(1%)",
+            opacity: "0",
+          }}
+          end={{ transform: "translateY(0%)", opacity: "1" }}
         >
-          {props.children}
-        </div>
-      </div>
-      <div className="flex flex-row gap-4 text-white">
-        <div className="flex lg:flex-col lg:items-center items-end gap-1">
-          <div>25</div>
-          <div className="text-sm text-[#aaa]">โพสต์</div>
-        </div>
-        <div className="lg:border-r-[1px] lg:border-[#aaa]" />
-        <div className="flex lg:flex-col lg:items-center items-end gap-1">
-          <div>3.6K</div>
-          <div className="text-sm text-[#aaa]">ข้อความ</div>
-        </div>
-        <div className="flex lg:flex-col lg:items-center items-end gap-1">
-          <div>แก้ไขล่าสุด</div>
-          <div className="text-sm text-[#aaa]">28/5/2565 - Admin</div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export function Header(props) {
-  return (
-    <div className="flex flex-row justify-start gap-2 items-center ">
-      <Link href={props.link}>
-        <a className="truncate text-white cursor-pointer hover:underline w-fit">
-          {props.children}
-        </a>
-      </Link>
-      <Badged text="ใหม่" />
-    </div>
-  );
-}
-
-export function Content(props) {
-  return (
-    <p className="truncate w-full text-[#aaa] text-xs">{props.children}</p>
-  );
-}
-
-function Badged({ text }) {
-  return (
-    <div className="px-1 bg-[#ec5555] w-fit h-fit rounded-sm text-white text-xs">
-      {text}
+          <div className="rounded-sm bg-[#242629] w-full ">
+            <h2
+              className={`flex flex-row justify-between items-center bg-[#ec5555] py-1 px-3 font-bold text-md text-white ${
+                toggle === true ? "rounded-t-sm" : "rounded-sm"
+              }`}
+            >
+              <Link href={postData.titleLink}>
+                <a>
+                  <span className="cursor-pointer hover:underline">
+                    {postData.title}
+                  </span>
+                </a>
+              </Link>
+              <div className="flex flex-row gap-2 ">
+                <Edit
+                  onClick={() => setOpenMenu(!openMenu)}
+                  className="hover:translate-y-[1px] hover:opacity-75 cursor-pointer"
+                />
+                <ChevronUp
+                  size={22}
+                  onClick={() => setTogle(!toggle)}
+                  className={`${
+                    toggle === false ? "rotate-180" : ""
+                  } cursor-pointer transition-all hover:translate-y-[1px] hover:opacity-75`}
+                />
+              </div>
+            </h2>
+            <div className="flex flex-col bg-[#aaa] gap-[1px]">
+              {toggle ? (
+                <Content
+                  header={postData.header}
+                  headerLink={postData.headerLink}
+                  body={postData.body}
+                />
+              ) : null}
+            </div>
+          </div>
+        </Animate>
+      </Container>
     </div>
   );
 }
--- pages/homePage.js
+++ pages/homePage.js
@@ -1,142 +1,50 @@
 import React from "react";
-import HomePost, { Body, Title, Header, Content } from "@components/HomePost";
-import { Container } from "@chakra-ui/react";
-import { Animate } from "react-simple-animate";
+import HomePost from "@components/HomePost";
+import { Button, Container, Tooltip } from "@chakra-ui/react";
+import { BookDownload, SquarePlus } from "tabler-icons-react";
+import { ViewOffIcon } from "@chakra-ui/icons";
+import ReactTooltip from "react-tooltip";
 
 export default function HomePage() {
+  const postDummy = [
+    {
+      title: "อัพเดทข่าวสาร",
+      titleLink: "/news",
+      header: "ข่าวสารอนิเมะอนิเมะ & ประกาศจากเว็บไซต์",
+      headerLink: "/news",
+      body: "โพสต์รวบรวมอนิเมะเปิดตัวใหม่ และข่าวสารต่างๆ เกี่ยวกับอนิเมะ",
+    },
+    {
+      title: "รีวิว อนิเมะ มังงะ สปอย",
+      titleLink: "/review",
+      header: "รีวิวอนิเมะเปิดตัวใหม่ และข้อมูลที่เกี่ยวข้อง",
+      headerLink: "/news",
+      body: "โพสต์รวบรวมรีวิวอนิเมะก่อนไปรับชม และเรื่องย่อต่างๆ พร้อมข้อมูลจำเพราะของตัวละคร ฯลฯ",
+    },
+    {
+      title: "Q&A ถาม-ตอบ ข้อสงสัยต่างๆ",
+      titleLink: "/qAndA",
+      header: "โพสต์ ถาม-ตอบ ข้อสงสัยเกี่ยวกับ อนิเมะ มังงะ",
+      headerLink: "/news",
+      body: "โพสต์รวบรวมรีวิวอนิเมะก่อนไปรับชม และเรื่องย่อต่างๆ พร้อมข้อมูลจำเพราะของตัวละคร ฯลฯ",
+    },
+  ];
   return (
     <div>
-      <div className="bg-img h-full">
+      <div className="h-full text-white">
         <Container maxW="container.xl">
-          <Animate
-            play
-            start={{
-              transform: "translateY(1%)",
-              opacity: "0",
-            }}
-            end={{ transform: "translateY(0%)", opacity: "1" }}
-          >
-            <div className="flex flex-col gap-2">
-              <HomePost>
-                <Title link="/news">อัพเดทข่าวสาร</Title>
-                <Body linkHeading="/">
-                  <Header link="/news">
-                    ข่าวสารอนิเมะอนิเมะ & ประกาศจากเว็บไซต์
-                  </Header>
-                  <Content>
-                    โพสต์รวบรวมอนิเมะเปิดตัวใหม่ และข่าวสารต่างๆ เกี่ยวกับอนิเมะ
-                  </Content>
-                </Body>
-              </HomePost>
-
-              <HomePost>
-                <Title link="/review">รีวิว อนิเมะ มังงะ สปอย</Title>
-                <Body linkHeading="/">
-                  <Header link="/review">
-                    รีวิวอนิเมะเปิดตัวใหม่ และข้อมูลที่เกี่ยวข้อง
-                  </Header>
-                  <Content>
-                    โพสต์รวบรวมรีวิวอนิเมะก่อนไปรับชม และเรื่องย่อต่างๆ
-                    พร้อมข้อมูลจำเพราะของตัวละคร ฯลฯ
-                  </Content>
-                </Body>
-              </HomePost>
-
-              <HomePost>
-                <Title link="/review">Q&A ถาม-ตอบ ข้อสงสัยต่างๆ</Title>
-                <Body linkHeading="/">
-                  <Header link="/review">
-                    โพสต์ ถาม-ตอบ ข้อสงสัยเกี่ยวกับ อนิเมะ มังงะ{" "}
-                  </Header>
-                  <Content>
-                    โพสต์รวบรวมรีวิวอนิเมะก่อนไปรับชม และเรื่องย่อต่างๆ
-                    พร้อมข้อมูลจำเพราะของตัวละคร ฯลฯ
-                  </Content>
-                </Body>
-              </HomePost>
-
-              <HomePost>
-                <Title link="/news">โพสต์ใหม่วันนี้</Title>
-                <Body linkHeading="/">
-                  <Header link="/news">
-                    สงสัยเกี่ยวกับตอนจบอนิเมะ Jujutsu Kaisen ครับ
-                  </Header>
-                  <Content>
-                    ขอออกตัวไว้ก่อนว่าผมอาจจะเก็บรายละเอียดไม่ครบก็ได้
-                    คือผมไม่เข้าใจบทสนทนาตอนจบของฟุชิงุโระกับคุงิซากิ...
-                  </Content>
-                </Body>
-                <Body linkHeading="/">
-                  <Header link="/news">
-                    ”ดาบพิฆาตอสูร” สุดยอดอนิเมะที่ต้องดูให้ได้สักครั้งใน NETFLIX
-                  </Header>
-                  <Content>
-                    คามาโดะ ทันจิโร่ หนุ่มธรรมดาที่มีดีด้านประสาทรับรู้กลิ่น
-                    (จมูกดีนั่นแหละ) กับหัวที่แข็งยังกับหิน ระหว่างที่เค้า...
-                  </Content>
-                </Body>
-                <Body linkHeading="/">
-                  <Header link="/news">
-                    Violet Evergarden: The Movie
-                    จดหมายฉบับสุดท้าย...แด่เธอผู้เป็นที่รัก
-                  </Header>
-                  <Content>
-                    ผมให้ 9.5/10 เลยใครเป็นแฟนอนิเมะเรื่องนี้อย่าลืมไปดูกันนะ
-                    เข้าฉายในโรงรอบพิเศษ 16 - 17 มกราคม และรอบปกติ...
-                  </Content>
-                </Body>
-              </HomePost>
-
-              <HomePost>
-                <Title link="/news">นายทุน</Title>
-                <Body linkHeading="/">
-                  <Header link="/news">บริจาคเพื่อเว็บไซต์</Header>
-                  <Content>
-                    บริจาคให้กับเราเพื่อสนับสนุนชุมชนคนรักอนิเมะให้สามารถสร้างสรรค์ชุมชนอนิเมะที่น่าอยู่ต่อไปได้
-                  </Content>
-                </Body>
-                <Body linkHeading="/">
-                  <Header link="/news">”ดาบพิแนวทางในการพัฒนาเว็บไซต์</Header>
-                  <Content>
-                    สามารถแนะนำเราได้
-                    เราพร้อมปรับปรุ่งแก้ไขเสมอเพื่อชุมชนคนรักอนิเมะ
-                  </Content>
-                </Body>
-              </HomePost>
-
-              <HomePost>
-                <Title link="/news">ฟีดแบค & รายงาน</Title>
-                <Body linkHeading="/">
-                  <Header link="/news">แจ้งปัญหาเว็บไซต์</Header>
-                  <Content>
-                    คุณสามารถรายงานปัญหาเว็บไซต์ทั้งหมดที่พบได้ที่นี่
-                  </Content>
-                </Body>
-                <Body linkHeading="/">
-                  <Header link="/news">ปัญหาที่ได้รับการแก้ไขแล้ว</Header>
-                  <Content>ปัญหาเว็บไซต์ทั้งหมดที่ได้รับการแก้ไขแล้ว</Content>
-                </Body>
-              </HomePost>
-
-              <HomePost>
-                <Title link="/news">คำถามที่พบบ่อย</Title>
-                <Body linkHeading="/">
-                  <Header link="/news">
-                    ปัญหาการเข้าสู่ระบบและการลงทะเบียน
-                  </Header>
-                  <Content>ทำไมฉันต้องลงทะเบียน?</Content>
-                </Body>
-                <Body linkHeading="/">
-                  <Header link="/news">ปัญหาการโพสต์</Header>
-                  <Content>
-                    ฉันจะสร้างหัวข้อใหม่หรือโพสต์ตอบกลับได้อย่างไร?
-                  </Content>
-                </Body>
-              </HomePost>
-            </div>
-            <div className="h-[400px]"></div>
-          </Animate>
+          <div className="flex flex-row justify-between w-full">
+            <h1 className="bg-[#ec5555] w-fit px-3 mb-2 rounded-sm cursor-pointer hover:translate-y-[1px] hover:opacity-75">
+              สร้าง +
+            </h1>
+          </div>
         </Container>
+        <div className="flex flex-col gap-2">
+          <HomePost dummyData={postDummy[0]} />
+          <HomePost dummyData={postDummy[1]} />
+          <HomePost dummyData={postDummy[2]} />
+        </div>
+        <div className="h-[400px]"></div>
       </div>
     </div>
   );
