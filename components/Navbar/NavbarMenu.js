eb0b2f1cf5899c1d8c7509b0e5c7b4ed422727f2
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2022-06-13 02:37:27 +0700

    fix: Missing "key"
    Error: Missing "key" prop for element in iterator  react/jsx-key

--- components/Navbar/NavbarMenu.js
+++ components/Navbar/NavbarMenu.js
@@ -7,11 +7,12 @@ export default function NavbarMenu({ page, menu }) {
     <div className="flex flex-row gap-5">
       {menu.map((item) => (
         <Animate
+          key={item.name}
           play
           start={{ transform: "translateX(5%)", opacity: "0" }}
           end={{ transform: "translateX(0)", opacity: "1" }}
         >
-          <Link key={item.name} href={item.path}>
+          <Link href={item.path}>
             <div
               className={`border-b-4 border-transparent hover:border-b-4 hover:border-[#FA5252] ${
                 item.path === page ? "border-[#FA5252]" : ""
